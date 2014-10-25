8ef70ed3a964d2e76079cd606e7bb6a94b0d6091
Martin Fischer <Arthur.Spooner@gmail.com>
2014-10-25 12:04:11 +0200

    Remerger

--- GscriptingGrailsPlugin.groovy
+++ GscriptingGrailsPlugin.groovy
@@ -1,13 +1,10 @@
 class GscriptingGrailsPlugin {
-    // the plugin version
     def version = "0.0.1"
     def grailsVersion = "2.2 > *"
     def title = "Gscripting Plugin"
     def author = "Frank Tröger"
     def authorEmail = "frnktrgr@gmail.com"
     def description = 'Run Groovy scripts in Grails'
-
-    // URL to the plugin's documentation
     def documentation = "https://github.com/frnktrgr/grails-gscripting" //"http://grails.org/plugin/gscripting"
     def license = "APACHE"
     def issueManagement = [ system: "GITHUB", url: "https://github.com/frnktrgr/grails-gscripting/issues" ]

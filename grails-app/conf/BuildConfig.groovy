f6035a5a80c9ad3af12436194ffccf294d6a1788
Frank Tröger <frnktrgr@gmail.com>
2015-08-23 11:31:11 +0200

    upgrade to grails 2.5.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -15,7 +15,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:1.0.3' {
+        build ':release:3.1.1', ':rest-client-builder:2.1.1' {
             export = false
         }
     }

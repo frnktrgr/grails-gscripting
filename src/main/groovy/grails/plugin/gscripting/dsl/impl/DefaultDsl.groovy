e969a24999ebcd79ddc6962b59ff13a3eff2861e
Frank Tröger <frnktrgr@gmail.com>
2017-02-07 16:25:50 +0100

    fixed DefaultDsl setProperty ctx.state

--- src/main/groovy/grails/plugin/gscripting/dsl/impl/DefaultDsl.groovy
+++ src/main/groovy/grails/plugin/gscripting/dsl/impl/DefaultDsl.groovy
@@ -8,15 +8,15 @@ import grails.plugin.gscripting.dsl.IContext;
 class DefaultDsl implements GroovyInterceptable {
 
 	def grailsApplication
-	
+
 	Map scriptParams
 	IContext ctx
 	ScriptRuntimeEnv sre
-	
+
 	def log
 
 	def app = [:]
-	
+
 	public DefaultDsl(def grailsApplication, Map scriptParams, IContext ctx, ScriptRuntimeEnv sre) {
 		this.grailsApplication = grailsApplication
 		this.scriptParams = scriptParams
@@ -28,7 +28,7 @@ class DefaultDsl implements GroovyInterceptable {
 			this.app.put(it.propertyName, grailsApplication.mainContext.getBean(it.propertyName))
 		}
 	}
-	
+
 	@Override
 	public Object invokeMethod(String name, Object args) {
 //		log.trace "Calling ${name} with ${args} ..."
@@ -43,25 +43,29 @@ class DefaultDsl implements GroovyInterceptable {
 //		log.trace "Calling ${name} took ${endTime-startTime}ms"
 		return ret
 	}
-	
+
 	@Override
 	public Object getProperty(String name) {
 //		this.log.trace "Getting ${name} ..."
-		if(name in DefaultDsl.metaClass.properties.name)
+		if(name in DefaultDsl.metaClass.properties.name) {
 			return  DefaultDsl.metaClass.getProperty(this, name)
-		if(name in DefaultContext.metaClass.properties.name)
+		} else if(name in DefaultContext.metaClass.properties.name) {
 			return DefaultContext.metaClass.getProperty(ctx, name)
-		//throw new MissingPropertyException(name, this.getClass());
-		ctx.state.get(name)
+		} else {
+			//throw new MissingPropertyException(name, this.getClass());
+			ctx.state.get(name)
+		}
 	}
-	
+
 	@Override
 	public void setProperty(String name, Object args) {
 //		log.trace "Setting ${name} with ${args} ..."
-		if(name in DefaultDsl.metaClass.properties.name)
+		if(name in DefaultDsl.metaClass.properties.name) {
 			DefaultDsl.metaClass.setProperty(this, name, args)
-		if(name in DefaultContext.metaClass.properties.name)
+		} else if(name in DefaultContext.metaClass.properties.name) {
 			DefaultContext.metaClass.setProperty(ctx, name, args)
-		ctx.state.put(name,args)
+		} else {
+			ctx.state.put(name,args)
+		}
 	}
 }
